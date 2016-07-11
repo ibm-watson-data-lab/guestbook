0c143cecc5cb1b081e661d50cdf332f3bad506c9
Lorna Jane Mitchell <lorna@lornajane.net>
2016-07-11 17:06:10 +0100

    Make node fetch couchdb data before processing queue using async

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,27 +1,36 @@
 var amqp = require('amqplib/callback_api');
-
-var hooks = [];
 var nano = require('nano')('http://localhost:5984');
-var webhooks_db = nano.use('webhooks');
-webhooks_db.view('main', 'all', function (err, hook) {
-    hook.rows.forEach(function (row) {
-        hooks.push(row.value.url);
-    });
-    console.log(hooks);
+var async = require('async');
+
+async.waterfall([
+    function(callback) {
+		var hooks = [];
+		var webhooks_db = nano.use('webhooks');
+		webhooks_db.view('main', 'all', function (err, hook) {
+			hook.rows.forEach(function (row) {
+				hooks.push(row.value.url);
+			});
+            callback(null, hooks);
+		});
+
+    },
+    function(hooks, callback) {
+        // console.log(hooks);
+
+        amqp.connect('amqp://localhost', function(err, conn) {
+            conn.createChannel(function(err, ch) {
+                var q = 'comments';
+
+                ch.assertQueue(q, {durable: true});
+                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+                ch.consume(q, function(msg) {
+                console.log(" [x] Received %s", msg.content.toString());
+                }, {noAck: true});
+            });
+        });
+    }
+], function (err, hooks) {
 });
 
 
 
-
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = 'comments';
-
-    ch.assertQueue(q, {durable: true});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(msg)
-      console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
-  });
-});
