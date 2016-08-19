1672e046b59153c4989ad1bc964d48e2ffe371c9
Lorna Jane Mitchell <lorna@lornajane.net>
2016-08-19 17:50:46 +0100

    Switch back to local rabbitmq

--- src/web/config.php
+++ src/web/config.php
@@ -5,6 +5,12 @@ $config['addContentLengthHeader'] = false;
 
 $config['couchdb']['url'] = "http://localhost:5984";
 
+$config['rabbitmq']['host'] = "localhost";
+$config['rabbitmq']['port'] = 5672;
+$config['rabbitmq']['username'] = "guest";
+$config['rabbitmq']['password'] = "guest";
+
+/*
 $config['rabbitmq']['host'] = "sl-eu-lon-2-portal.2.dblayer.com";
 $config['rabbitmq']['port'] = 10406;
 $config['rabbitmq']['vhost'] = "lj-brilliant-rabbitmq";
@@ -12,3 +18,4 @@ $config['rabbitmq']['username'] = "lorna";
 $config['rabbitmq']['password'] = "password";
 $config['rabbitmq']['ssl'] = true;
 $config['rabbitmq']['cert'] = '../certrabbit';
+ */
