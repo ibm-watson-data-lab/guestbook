f5455eca52c5fc2f9dfb896781ea116492edc934
Lorna Jane Mitchell <lorna@lornajane.net>
2018-02-09 17:16:05 +0000

    Use php-couchdb lib, zip to include dependencies, switch to raw request

---
+++ src/serverless-webhook-receiver/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function main(array $params) : array
+{
+    echo "Hello world\n";
+    $db_url = $params['cloudantURL'];
+
+    $incoming_body = base64_decode($params['__ow_body']);
+    $data = json_decode($incoming_body, true);
+
+    echo "Saving data ...\n";
+    $server = new \PHPCouchDB\Server(["url" => $db_url]);
+    $db = $server->useDb(["name" => "incoming"]);
+
+    $meta = ["received" => time(), "status" => "new"]; 
+    $db->create(["data" => $data, "meta" => $meta]);
+    return ["body" => "Exxxxxcellent"];
+}
